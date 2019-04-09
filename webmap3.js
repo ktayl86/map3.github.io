8b9e19e8a7d169a19831a8e0f495cff9c0211ef8
ktayl86 <48958922+ktayl86@users.noreply.github.com>
2019-04-09 02:47:57 -0500

    Add files via upload

--- webmap3.js
+++ webmap3.js
@@ -1,12 +1,20 @@
 let stateMap = L.map('map3').setView([32.18, -99.14], 4)
-let basemapUrl = 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+let basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 L.tileLayer(basemapUrl).addTo(stateMap)
+let streetsBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(stateMap)
+let grayBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(stateMap)
+let basemaps = {
+  'Streets': streetsBasemap,
+  'Gray canvas': grayBasemap
+}
+L.control.layers(basemaps).addTo(stateMap)
+
 let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(stateDemographicsUrl, function (data) {
   let stateStyle = function (feature) {
-    let age = feature.properties.MED_AGE // get the current state's Median Age attribute
+    let population = feature.properties.POPULATION // get the current state's population attribute
     let stateColor = 'olive' // let the initial color be a darker green
-    if (age < 38) { stateColor = 'green' } // if the state's median age is less than the average, color it a lighter green
+    if (population < 100000) { stateColor = 'green' } // if the state's population is less than the average, color it a lighter green
     return {
       color: stateColor, // use the color variable above for the value
       weight: 1,
@@ -15,8 +23,8 @@ jQuery.getJSON(stateDemographicsUrl, function (data) {
   }
   let onEachFeature = function (feature, layer) {
     let name = feature.properties.STATE_NAME
-    let age = feature.properties.MED_AGE
-    layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+    let population = feature.properties.POPULATION
+    layer.bindPopup('Population of ' + name + ': ' + population + '<br>National average: 38')
   }
   let geojsonOptions = {
     style: stateStyle,
